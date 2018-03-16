6b166581b6b9dd1ba847c78b11ee649309b0b37d
Perin <joaovperin@live.com>
2018-03-16 15:31:46 -0300

    Added JSOUP to generate ADs.

--- src/br/feevale/perin/semaphoro/AdvertisementGenerator.java
+++ src/br/feevale/perin/semaphoro/AdvertisementGenerator.java
@@ -4,6 +4,11 @@
  */
 package br.feevale.perin.semaphoro;
 
+import java.io.IOException;
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+
 /**
  * Descrição da classe.
  */
@@ -15,25 +20,27 @@ public class AdvertisementGenerator {
      * @return String
      */
     public static String randomAd() {
-//        try {
-//            // Request to FreeMarket.
-//            Document doc = Jsoup.connect("https://www.mercadolivre.com.br/").get();
-//            // Recommendations section
-//            Element sectionRecommendations = doc.select("section.recommendations").first();
-//            // ADs
-//            StringBuilder sb = new StringBuilder(1024);
-//            sb.append(doc.title()).append("\n");
-//            sb.append("Confira as ofertas que estão bombando na sua cidade!!").append("\n\n\n");
-//            // Each element
-//            sectionRecommendations.select("div.slick-slide.slick-active").stream().forEach((e) -> {
-//                Element link = e.select("a[href]").first();
-//                sb.append(link.text()).append('\n').append(link.attr("href")).append("\n\n");
-//            });
-//            return sb.append("\n").toString();
-//        } catch (IOException e) {
-//            return "randomAd";
-//        }
-        return "randomAd";
+        try {
+            System.out.println("randomAD");
+            // Request to FreeMarket.
+            Document doc = Jsoup.connect("https://www.mercadolivre.com.br/").get();
+            // Recommendations section
+            Element sectionRecommendations = doc.select("section.recommendations").first();
+            // ADs
+            StringBuilder sb = new StringBuilder(1024);
+            sb.append(doc.title()).append("\n");
+            sb.append("Confira as ofertas que estão bombando na sua cidade!!").append("\n\n\n");
+            // Each element
+            sectionRecommendations.select("div.slick-slide.slick-active").stream().forEach((e) -> {
+                Element link = e.select("a[href]").first();
+                sb.append(link.text()).append('\n').append(link.attr("href")).append("\n\n");
+            });
+            return sb.append("\n").toString();
+        } catch (IOException e) {
+            System.out.println("exception jsoup:");
+            System.out.println(e);
+            return "randomAd";
+        }
     }
 
 }
